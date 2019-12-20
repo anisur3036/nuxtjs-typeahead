87c67b97013e6d91afc61094a602914e3972e5f6
Anisur Rahman <anisurlist3@gmail.com>
2019-12-20 21:35:00 +0600

    Project Setup

---
+++ components/Typeahead.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="typeahead">
+		<div class="typeahead__toggle" ref="toggle">
+			<input type="text" class="typeahead__search" ref="search">
+			<span class="typeahead__text" ref="text">Sample Text</span>
+		</div>
+		<u class="typeahead__list">
+			<li class="typeahead__item">
+				<a class="typeahead__link">Test Option 1</a>
+			</li>
+			<li class="typeahead__item">
+				<a class="typeahead__link">Test Option 2</a>
+			</li>
+		</u>
+	</div> 
+</template>
+
+<script>
+export default {
+	name: 'Typeahead'
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Typeahead />
+  </div>
+  
+</template>
+
+<script>
+
+import Typeahead from '@/components/Typeahead.vue'
+
+export default {
+  components: {
+    Typeahead
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
