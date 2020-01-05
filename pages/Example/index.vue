0e35022de45691049e4e55ad1760dfb66b985e8e
Anisur Rahman <anisurlist3@gmail.com>
2020-01-05 20:15:58 +0600

    px to rem convert using scss

---
+++ pages/Example/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="px">This is 22 px</div>
+    <div id="rem">This is 22 rem</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@mixin font-size($pixels) {
+  $rem: ($pixels * 0.0625);
+  font-size: $rem + rem;
+  font-size: $pixels + px;
+}
+
+#px {
+  font-size: 22px;
+}
+
+#rem {
+  @include font-size(22);
+}
+</style>
